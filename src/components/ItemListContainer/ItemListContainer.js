08e6e9442987b3a915817ccdc10e6728ce1d53b8
Nicolas Tosoroni <nicolastosoroni@gmail.com>
2021-06-10 01:31:08 -0300

    correcciones desafio 4 y agragado de 5

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import ItemCount from '../Counter/ItemCount'
 
 const ItemListContainer = () => {
+    
+    const onAdd = (amount) => {
+        alert(`Compraste ${amount} productos`)
+    }
+    
     return (
         <div>
-            <h1>Proximamente, muchos Legos</h1>
+            <h1>Proximamente, muchos Legoss</h1>
+            <ItemCount initial={1} stock={10} onAdd={onAdd}/>
         </div>
     )
 }
