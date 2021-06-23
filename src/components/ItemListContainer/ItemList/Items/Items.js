394df98fab4bd3748358e512db625690150534d8
Nicolas Tosoroni <nicolastosoroni@gmail.com>
2021-06-23 02:34:51 -0300

    Desafio 6. Intento de implementar setTimeout y primise

--- src/components/ItemListContainer/ItemList/Items/Items.js
+++ src/components/ItemListContainer/ItemList/Items/Items.js
@@ -1,6 +1,41 @@
 import React from 'react';
 
-const data = [
+// const allProducts = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       () =>
+//         resolve([
+//           {
+//             id: 1,
+//             nombre: 'producto1',
+//             detalle: 'Este es el detalle del producto1',
+//             destacado: true,
+//           },
+//           {
+//             id: 2,
+//             nombre: 'producto2',
+//             detalle: 'Este es el detalle del producto2',
+//             destacado: true,
+//           },
+//           {
+//             id: 3,
+//             nombre: 'producto3',
+//             detalle: 'Este es el detalle del producto3',
+//             destacado: false,
+//           },
+//           {
+//             id: 4,
+//             nombre: 'producto4',
+//             detalle: 'Este es el detalle del producto4',
+//             destacado: false,
+//           },
+//         ]),
+//       2000
+//     );
+//   });
+// };
+
+const allProducts = [
   {
     id: 1,
     nombre: 'producto1',
@@ -27,16 +62,20 @@ const data = [
   },
 ];
 
-export const Items = () => {
+const Producto = ({ titulo, texto, i }) => {
   return (
-    <div className="list2Container">
-      {data.map((element, i) => (
-        <React.Fragment key={i}>
-          <h4>{element.nombre}</h4>
-          <p>{element.detalle}</p>
-          <button>Comprar</button>
-        </React.Fragment>
-      ))}
-    </div>
+    <React.Fragment key={i}>
+      <h4>{titulo}</h4>
+      <p>{texto}</p>
+      <button>Comprar</button>
+    </React.Fragment>
   );
+};
+
+export const Items = () => {
+  //   const array = <------ no se como obtner el array de la promise
+  const listaProductos = allProducts.map((element, i) => (
+    <Producto titulo={element.nombre} texto={element.detalle} i={i} />
+  ));
+  return <div className="list2Container">{listaProductos}</div>;
 };
