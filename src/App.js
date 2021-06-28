a9585bc30e058d5d85f7946173b864af7084a99d
Nicolas Tosoroni <nicolastosoroni@gmail.com>
2021-06-28 02:23:51 -0300

    Agregado Home y Cart con routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer />
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/list">
+            <ItemListContainer />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
