bdb2068c68bb65875c2261cfb0eefbecbd0969cf
Nicolas Tosoroni <nicolastosoroni@gmail.com>
2021-06-28 02:26:45 -0300

    Agregado Home y Cart con routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ const NavBar = () => {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">e-comToso</Navbar.Brand>
+        <Navbar.Brand href="/">e-comToso</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#products">Products</Nav.Link>
+            <Nav.Link href="/list">Products</Nav.Link>
             <NavDropdown title="Categories" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#castle">Castles</NavDropdown.Item>
               <NavDropdown.Item href="#pirate">Pirates</NavDropdown.Item>
